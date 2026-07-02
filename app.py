b6ed9b5710eaa9ce8ebdb75b4ebda3b88f16ed57
laksh-1901 <lakshmilaksh1901@gmail.com>
2026-07-02 07:19:58 +0000

    Add website health monitoring

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from modules.monitor import get_system_info
 from modules.services import get_services
 from modules.website_monitor import check_website
 
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -10,15 +11,12 @@ def home():
 
     system = get_system_info()
     services = get_services()
-
-    website = check_website("http://127.0.0.1:5000")
-
+    website = check_website("https://www.google.com")
     return render_template(
-        "index.html",
-        system=system,
-        services=services,
-        website=website
-    )
-
+    "index.html",
+    system=system,
+    services=services,
+    website=website
+)
 if __name__ == "__main__":
     app.run(debug=True)
--- modules/website_monitor.py
+++ modules/website_monitor.py
@@ -4,23 +4,24 @@ import time
 
 def check_website(url):
     try:
-        start = time.time()
+        start = time.perf_counter()
 
         response = requests.get(url, timeout=5)
 
-        end = time.time()
-
-        response_time = round((end - start) * 1000, 2)
+        response_time = round((time.perf_counter() - start) * 1000, 2)
 
         return {
+            "url": url,
             "status": "Healthy",
             "status_code": response.status_code,
             "response_time": response_time
         }
 
     except requests.exceptions.RequestException:
+
         return {
+            "url": url,
             "status": "Down",
-            "status_code": "N/A",
-            "response_time": "N/A"
+            "status_code": "-",
+            "response_time": "-"
         }
